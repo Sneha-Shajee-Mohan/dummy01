50ced333c40dd2e3fb2382c88a454ad8f139f1d9
Sneha-Shajee-Mohan <156137687+Sneha-Shajee-Mohan@users.noreply.github.com>
2024-11-06 10:01:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from dummy01 import report_count
 
-if __name__ == "__main__":
+def main ():
     token = input("Enter the token to search for in the corpus: ")
     report_count(token)
+if __name__ == "__main__":
+    main()
